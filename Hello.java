c9a83a1b9e3ea614b83fea5e31e92d832062518e
javawebusingapi <maves951@gmail.com>
2022-04-01 14:14:02 +0900

    add Good

--- Hello.java
+++ Hello.java
@@ -2,10 +2,11 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Gits!");
 		System.out.println("login");
-		system.out.println("logout");
-		system.out.println("Register");
-		system.out.println("what is Git?");
-		system.out.println("That's DVCS!");
+		System.out.println("logout");
+		System.out.println("Register");
+		System.out.println("what is Git?");
+		System.out.println("That's DVCS!");
+		System.out.println("Good Afternoon!");
 	}
 }
 
